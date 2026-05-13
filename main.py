523bd82ffd1b7652208725dadc03ea37f07f8f75
sabdanio <sabdanio007@gmail.com>
2026-05-13 11:31:31 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def run_bot():
     os.system("python bot.py")
 
 def run_api():
-    os.system("python -m uvicorn api:app --host 0.0.0.0 --port 8000")
+    os.system("python -m uvicorn app:api --host 0.0.0.0 --port 8000")
 
 if __name__ == "__main__":
     p1 = Process(target=run_bot)
